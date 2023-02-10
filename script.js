0fffc1fd1e9366cdf630558ee72f4956b400e9e2
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-02-11 00:20:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+const cart = ["shoes","pants","shirts"]
+
+// apis 
+createOrder
+Payment
+orderSummary
+Wallet
+
+api.createOrder(cart, function () {
+  // create order api
+  api.Payment(payment,function()) {
+  //  paymentApi
+     api.orderSummary(order,function()) {
+  //  orderSummary
+     api.wallet() {
+     }
+}
+}
+  )
+
+// we are giving control to the create order function, what if that function is wrong
+
+
+let time = document.getElementById("timer");
+
+setTimeout(() => {
+  time.innerText = "10";
+  setTimeout(() => {
+    time.innerText = "9";
+    setTimeout(() => {
+      time.innerText = "8";
+      setTimeout(() => {
+        time.innerText = "7";
+        setTimeout(() => {
+          time.innerText = "6";
+          setTimeout(() => {
+            time.innerText = "5";
+            setTimeout(() => {
+              time.innerText = "4";
+              setTimeout(() => {
+                time.innerText = "3";
+                setTimeout(() => {
+                  time.innerText = "2";
+                  setTimeout(() => {
+                    time.innerText = "1";
+                    setTimeout(() => {
+                      time.innerText = "Happy Independence day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+
+
